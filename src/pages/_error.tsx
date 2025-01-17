7d42044de27542ec8e6d3c21d604fe6942001088
mehmetozdursun <142603171+mehmetozdursun@users.noreply.github.com>
2025-01-17 20:17:59 +0300

    event trackers

---
+++ src/pages/_error.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const CustomErrorPage = ({ statusCode }: { statusCode: number }) => {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (statusCode === 404) {
+            // Redirect to the custom NotFoundPage for 404 errors
+            router.replace("/NotFoundPage");
+        } else {
+            // For other errors, you can choose to show a fallback or redirect elsewhere
+            router.replace("/");
+        }
+    }, [statusCode, router]);
+
+    return ''; // No need to render anything here, just redirect
+};
+
+CustomErrorPage.getInitialProps = async ({ res, err }: { res: any; err: any }) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+    return { statusCode };
+};
+
+export default CustomErrorPage;
