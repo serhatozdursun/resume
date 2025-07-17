5013ef375d16756f714d3047e6a6a6b77c32da43
mehmetozdursun <serhat.ozdursun@gmail.com>
2025-07-17 22:39:18 +0300

    update tests

--- src/tests/StyledComponents.test.tsx
+++ src/tests/StyledComponents.test.tsx
@@ -14,34 +14,54 @@ import { ThemeProvider } from 'styled-components';
 
 describe('StyledComponents', () => {
   it('renders Container correctly', () => {
-    const { container } = render(<Container />);
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Container />
+      </ThemeProvider>
+    );
     expect(container.firstChild).toHaveStyleRule('display', 'flex');
     expect(container.firstChild).toHaveStyleRule('max-width', '100%');
   });
 
   it('renders LeftColumn correctly', () => {
-    const { container } = render(<LeftColumn />);
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <LeftColumn />
+      </ThemeProvider>
+    );
     expect(container.firstChild).toHaveStyleRule('flex', '1');
     expect(container.firstChild).toHaveStyleRule('padding-top', '40px');
   });
 
   it('renders RightColumn correctly', () => {
-    const { container } = render(<RightColumn />);
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <RightColumn />
+      </ThemeProvider>
+    );
     expect(container.firstChild).toHaveStyleRule('flex', '2.8');
     expect(container.firstChild).toHaveStyleRule('padding-right', '100px');
     expect(container.firstChild).toHaveStyleRule('padding-left', '30px');
   });
 
   it('renders Name correctly', () => {
-    const { container } = render(<Name />);
-    expect(container.firstChild).toHaveStyleRule('font-size', '2.5em');
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Name />
+      </ThemeProvider>
+    );
+    expect(container.firstChild).toHaveStyleRule('font-size', '1.5em');
     expect(container.firstChild).toHaveStyleRule('margin-bottom', '0.2em');
   });
 
   // Add similar tests for other components as needed
 
   it('SkillLevelFill renders with correct width', () => {
-    const { container } = render(<SkillLevelFill $level={50} />);
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <SkillLevelFill $level={50} />
+      </ThemeProvider>
+    );
     const skillLevelFillElement = container.firstChild as HTMLElement;
     expect(skillLevelFillElement).toHaveStyleRule('width: 50%;');
   });
