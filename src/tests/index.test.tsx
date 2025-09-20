d32a90f24c89960560b082d8c2aab927d9b5cba4
serhatozdursun <serhat.ozdursun@gmail.com>
2025-09-20 13:08:08 +0300

    headline

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -136,7 +136,7 @@ describe('IndexPage Component', () => {
 
       const title = document.querySelector('title');
       expect(title).toHaveTextContent(
-        'Mehmet Serhat Özdursun - QA Automation Engineer'
+        'Mehmet Serhat Özdursun - QA Automation Leader | ISTQB CTAL-TAE Certified | Driving Test Automation Excellence for 13+ Years'
       );
     });
 
